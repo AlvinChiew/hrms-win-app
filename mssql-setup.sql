dbca559de1dbcdec5c77323d7f48302cd2ac202c
alvinchiew <jingjie95@gmail.com>
2022-08-13 00:53:01 +0800

    code checkpoint

---
+++ mssql-setup.sql
@@ -0,0 +1,42 @@
+CREATE DATABASE dbHrmsProj;
+
+CREATE TABLE dbHrmsProj.dbo.tblEmployees (
+    ID BIGINT NOT NULL PRIMARY KEY IDENTITY(10001,1),
+    FirstName VARCHAR(255),
+    LastName VARCHAR(255),
+    MiddleName VARCHAR(255),
+    PositionID BIGINT,
+    Gender VARCHAR(18),
+    BDate DATE,
+    HomeAddress VARCHAR(255),
+    TelNo VARCHAR(255),
+    MobileNo VARCHAR(255),
+    EmailAddress VARCHAR(255)
+)
+
+CREATE TABLE dbHrmsProj.dbo.tblPosition (
+    PositionID BIGINT NOT NULL PRIMARY KEY IDENTITY(10001,1),
+    PositionName VARCHAR(255)
+)
+
+INSERT INTO dbHrmsProj.dbo.tblPosition (PositionName) 
+VALUES ('CEO'), ('CTO'), ('Admin'), ('Engineers'), ('Marketing Executive')
+
+INSERT INTO dbHrmsProj.dbo.tblEmployees (
+    FirstName,
+    LastName,
+    MiddleName,
+    PositionID,
+    Gender,
+    BDate,
+    HomeAddress,
+    TelNo,
+    MobileNo,
+    EmailAddress
+) VALUES 
+    ('Alvin', 'Chiew', NULL, 10001, 'Male', '1992-02-22', 'Kuala Lumpur, Malaysia', '+6031324523', '+60181523523', 'alvin@email.com'),
+    ('Melvin', 'Cheong', 'Great', 10002, 'Male', '1982-04-30', 'Kedah, Malaysia', NULL, NULL, 'melvin@email.com')
+
+
+SELECT * FROM dbHrmsProj.dbo.tblPosition;
+SELECT * FROM dbHrmsProj.dbo.tblEmployees;
---
+++ src/Form1.cs
@@ -0,0 +1,9 @@
+namespace hrms_win_app;
+
+public partial class Form1 : Form
+{
+    public Form1()
+    {
+        InitializeComponent();
+    }
+}
